1ad4709a0aca49da78746e31703c19fde2325bd2
Scott <scotttherocker@gmail.com>
2015-08-06 22:09:43 -0600

    more swift playgrounds

--- Playgrounds/MyPlayground.playground/Contents.swift
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -3,3 +3,20 @@
 import UIKit
 
 var str = "Hello, playground"
+var num = 13
+var name:String = "Scott"
+name = "Carina"
+
+var nextNum = num * 5
+var a:Int = 45
+var newNum:Double = 2.45
+
+Double(a) * newNum
+
+var longSent = str + " " + name + " \(a)"
+Int(newNum)
+
+var numOne = 3
+var numTwo = 4.5
+var answer = Double(numOne) * numTwo
+var sentence = "\(numOne) times \(numTwo) is equal to \(answer)"
